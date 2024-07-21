3628d06875fe4b90ab7faae0f7055d06792eb356
= <yashbansodgit@gmail.com>
2024-07-21 15:45:07 +0530

    feat: configure bash_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 echo "BUILD START"
 python3.9 -m pip install -r requirements.txt
+python3.9 manage.py migrate --noinput
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
 

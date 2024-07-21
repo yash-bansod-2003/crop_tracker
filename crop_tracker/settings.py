4ccfc860807f3bf052e0a8e874050e456054dc3c
= <yashbansodgit@gmail.com>
2024-07-21 15:28:20 +0530

    feat: build_files.sh configure

--- crop_tracker/settings.py
+++ crop_tracker/settings.py
@@ -135,3 +135,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static') ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles' , 'static' )
